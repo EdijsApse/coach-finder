fa593ee5a1083e498663b3bf977591dfb7b4518e
Edijs <edijsapse@gmail.com>
2022-07-29 09:59:31 +0300

    axios & CORS

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const coachRoutes = require('./coachRoutes');
 
+router.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 router.use('/coaches', coachRoutes);
 
 module.exports = router
